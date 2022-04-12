69e12f42da1d8a7ec7d74ab0a98a86f274852a5a
salsabilamariska <103508210+salsabilamariska@users.noreply.github.com>
2022-04-12 05:26:35 -0700

    TUGAS01

---
+++ konversi.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Konversi suhu</title>
+</head>
+<body>
+   <form action="hasil.php" method="post">
+       <h1>Konversi suhu</h1>
+       Konversi: <br>
+       <input type="radio" name="konversi" value="celcius ke fahrenhait">celcius ke fahrenhait
+       <input type="radio" name="Konversi" value="fahrenhait ke celcius">Fahrenhait ke celcius <br>
+       <br>
+       suhu<input type="text" name="suhu"><br><br>
+       <hr>
+       <input type="submit" name="submit" value="konveresi" >
+   </form>
+   
+</body>
+</html>
